8066c02d0f970c57466dee4bbc8006972373723c
keyur diwan <keyurdiwan009@gmail.com>
2022-01-04 07:02:42 +0530

    work on  layout!

---
+++ client/src/Components/ChannelContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChannelContainer() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChannelContainer
---
+++ client/src/Components/ChannelListContainer.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './index.js';
+// import HospitalIcon from '../assets/hospital.png';
+// import LogoutIcon from '../assets/logout.png';
+
+const HospitalIcon = require('../assets/hospital.png');
+const LogoutIcon = require('../assets/logout.png');
+const SideBar = () => (
+    <div className="channel-list__sidebar">
+
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={HospitalIcon} alt="Hostpital" width="30" />
+            </div>
+        </div>
+
+
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt="Logout" width="30" />
+            </div>
+        </div>
+    </div>
+)
+
+const CompanyHeader = () => (
+    <div className="channel-list__header">
+
+        <p className="channel-list__header__text"> Slack clone </p>
+    </div>
+
+)
+
+function ChannelListContainer() {
+    return (
+        <>
+            <SideBar />
+            <div className="channel-list__list__wrapper">
+                <CompanyHeader />
+                <ChannelSearch />
+              
+                <ChannelList
+                    filters={{}}
+                   channelRenderFilterFn={ () => {}}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type='team'
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type='team'
+                        />
+                    )}
+                />
+
+
+                 <ChannelList
+                    filters={{}}
+                   channelRenderFilterFn={ () => {}}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type='messaging'
+                        />
+                    )}
+                    Preview={(previewProps) => (
+                        <TeamChannelPreview
+                            {...previewProps}
+                            type='messaging'
+                        />
+                    )}
+                />
+            </div>
+        </>
+    )
+}
+
+export default ChannelListContainer
